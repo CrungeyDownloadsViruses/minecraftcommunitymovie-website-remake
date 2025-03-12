a2e189768a3bc944cd6a289892daa6b971722845
CrungeyDownloadsViruses <76408831+CrungeyDownloadsViruses@users.noreply.github.com>
2025-03-12 07:14:08 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,44 @@ const axios = require('axios');
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+
+// add the stealth plugin
+puppeteer.use(StealthPlugin());
+
+(async () => {
+    // set up browser environment
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+
+    // navigate to a URL
+    await page.goto('http://minecraftcommunitymovie.com', {
+        waitUntil: 'networkidle0',
+    });
+
+    const html = await page.content();
+
+    console.log(html.split(`<span class="text-secondary-300">`)[1].split(`</span>`)[0]);
+    console.log(html.split(`<p>`)[2].split(`</p>`)[0]);
+    console.log(html.split(`<span class="text-sm text-secondary-300">`)[1].split(`</span>`)[0])
+    
+    //repeat 21 times
+    for (let i = 0; i < 21; i++) {
+        console.log(html.split(`<p class="text-secondary-300 text-center">`)[i + 1].split(`</p>`)[0] + ": " + html.split(`<p class="text-lg font-medium text-secondary-350">`)[i + 1].split(`</p>`)[0]);
+    }
+    // wait for the challenge to resolve
+    await new Promise(function (resolve) {
+        setTimeout(resolve, 10000);
+    });
+
+    // take page screenshot
+    //await page.screenshot({ path: 'screenshot.png' });
+
+    // close the browser instance
+    //await browser.close();
+})();
+
 
 //on go to 127.1.1:3090/ display index.html
 const app = express();
